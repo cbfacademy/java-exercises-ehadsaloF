984391e1ba129848d20930559549a43a8d3a56d5
Folashade Oyeleke <oyelekefola@yahoo.com>
2023-09-04 14:23:57 +0100

    Completed Primitive Exercise

--- exercises/java-primitives/src/main/java/com/cbfacademy/StringExercises.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/StringExercises.java
@@ -4,17 +4,34 @@ public class StringExercises {
 
     public String fromCharacters(char[] characters) {
         // TODO - Write code that creates a String from the input array of characters
-        throw new RuntimeException("Not implemented");
+        String finals = "";
+        for (char character : characters){
+            finals = finals + character;
+        }
+        return finals.toString();
     }
 
     public Long howMany(String text, Character character) {
         // TODO - Write code to determine how many of the input ${character} are contained in the input ${text}
-        throw new RuntimeException("Not implemented");
+        Long cnter = 0L;
+        for(char i : text.toCharArray()){
+            if(i == character) {
+                cnter++;
+            }
+        }
+        return cnter;
     }
 
     public Boolean isPalindrome(String word) {
         // TODO - Write code to determine whether the input ${word} is a palindrome
-        throw new RuntimeException("Not implemented");
+        for (char i : word.toCharArray()){
+            for(int j = word.length()-1; j >= 0; j--){
+                if( (word.indexOf(i) + j == word.length()-1) && (i != word.charAt(j))){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public String getName() {
